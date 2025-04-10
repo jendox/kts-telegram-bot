855498cd8cd823a321a948f409cab593c9325fe2
jendox <jendox1985@gmail.com>
2025-04-10 15:56:43 +0300

    added rabbitmq publish & consume

--- shared/rabbitmq_broker.py
+++ shared/rabbitmq_broker.py
@@ -1,5 +1,8 @@
 import os
+from asyncio import CancelledError
+from collections.abc import Callable
 from logging import getLogger
+from typing import Any
 
 import aio_pika
 
@@ -18,15 +21,18 @@ class RabbitMQBroker:
         self.channel: aio_pika.Channel | None = None
         self.queue: aio_pika.Queue | None = None
 
-    async def start(self):
+    async def start(self) -> None:
+        """Создает соединение, канал и декларирует очередь"""
         try:
             if not self.connection:
                 self.connection = await aio_pika.connect_robust(url=self.url)
+                self.logger.info("RabbitMQ connection successfully established")
             if not self.channel:
                 self.channel = self.connection.channel()
+                self.logger.info("RabbitMQ channel successfully created")
             if not self.queue:
                 self.queue = self.channel.declare_queue(self.queue_name, durable=True)
-            self.logger.info("RabbitMQ connection successfully established")
+                self.logger.info("RabbitMQ queue declared successfully")
         except aio_pika.exceptions.AMQPConnectionError as e:
             self.logger.error("RabbitMQ connection error: %s", str(e))
             raise
@@ -34,9 +40,51 @@ class RabbitMQBroker:
             self.logger.error("Unexpected starting RabbitMQ error: %s", str(e))
             raise
 
-    async def stop(self):
+    async def stop(self) -> None:
+        """Закрывает канал и соединение"""
         if self.channel:
             await self.channel.close()
+            self.logger.info("RabbitMq channel closed")
         if self.connection:
             await self.connection.close()
             self.logger.info("RabbitMq connection closed")
+
+    async def publish(self, message: str) -> None:
+        """Публикует сообщение в очередь
+        Args:
+            message (str): сообщение для публикации
+        """
+        if not self.connection:
+            await self.start()
+        try:
+            await self.channel.default_exchange.publish(
+                routing_key=self.queue_name,
+                message=aio_pika.Message(
+                    body=message.encode(MESSAGE_ENCODING),
+                    delivery_mode=aio_pika.DeliveryMode.PERSISTENT
+                )
+            )
+            self.logger.info("Message successfully published\nMessage: %s", message)
+        except Exception as e:
+            self.logger.error("Unexpected error publishing message: %s", str(e))
+
+    async def consume(self, callback: Callable[[str], Any]) -> None:
+        """Читает сообщение из очереди
+        Args:
+            callback: функция для обработки принятого сообщения
+        """
+        if not self.queue:
+            await self.start()
+
+        async with self.queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                try:
+                    async with message.process():
+                        await callback(message.body.decode(MESSAGE_ENCODING))
+                        # await message.ack() # for test only
+                except CancelledError:
+                    self.logger.info("Message consumption cancelled")
+                    raise
+                except Exception as e:
+                    self.logger.error("Error handling consuming message: %s", str(e))
+                    await message.nack(requeue=False)
