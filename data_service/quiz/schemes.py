7647f30d0606d837e0c33df4786f099eef97349e
jendox <jendox1985@gmail.com>
2025-04-11 22:33:31 +0300

    added quiz schemes & question views

---
+++ data_service/quiz/schemes.py
@@ -0,0 +1,28 @@
+from marshmallow import Schema, fields
+
+
+class UserSchema(Schema):
+    id = fields.Int(required=False)
+    telegram_id = fields.Str(required=True)
+    username = fields.Str(required=False)
+    full_name = fields.Str(required=False)
+
+
+class AnswerSchema(Schema):
+    title = fields.Str(required=True)
+    points = fields.Int(required=True)
+
+
+class QuestionSchema(Schema):
+    id = fields.Int(required=False)
+    title = fields.Str(required=True)
+    answers = fields.Nested(AnswerSchema, required=True, many=True)
+
+
+class ListQuestionSchema(Schema):
+    questions = fields.Nested(QuestionSchema, many=True)
+
+
+class ChatStateSchema(Schema):
+    chat_id = fields.Str(required=True)
+    state = fields.Str(required=True)
--- data_service/quiz/views.py
+++ data_service/quiz/views.py
@@ -1,5 +1,26 @@
-from data_service.web.app import Request
+from http import HTTPStatus
+
+from aiohttp.web_response import json_response
+from aiohttp_apispec import response_schema
+
+from data_service.quiz.schemes import ListQuestionSchema, QuestionSchema
+from data_service.web.app import View
 
 
-async def get_random_question(request: Request):
-    accessor = request.app.store
+class QuestionListView(View):
+    @response_schema(ListQuestionSchema)
+    async def get(self):
+        questions = await self.store.question_accessor.get_all_questions()
+        return json_response(
+            data=ListQuestionSchema().dump({"questions": questions}),
+            status=HTTPStatus.OK
+        )
+
+
+class QuestionRandomView(View):
+    @response_schema(QuestionSchema)
+    async def get(self):
+        question = await self.store.question_accessor.get_random_question()
+        return json_response(
+            data=QuestionSchema().dump(question)
+        )
